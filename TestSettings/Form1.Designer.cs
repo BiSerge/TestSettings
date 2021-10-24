f53f474c32c16899a68c5cfcd8967d512c5e338f
BiSerge <bichevoi@gmail.com>
2021-10-24 10:35:42 +0600

    Добавьте файлы проекта.

---
+++ TestSettings/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace TestSettings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::TestSettings.Properties.Settings.Default, "mText", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textBox1.Location = new System.Drawing.Point(22, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = global::TestSettings.Properties.Settings.Default.mText;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Цвет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = global::TestSettings.Properties.Settings.Default.mColor;
+            this.ClientSize = global::TestSettings.Properties.Settings.Default.mSize;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.DataBindings.Add(new System.Windows.Forms.Binding("Location", global::TestSettings.Properties.Settings.Default, "mLocation", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.DataBindings.Add(new System.Windows.Forms.Binding("ClientSize", global::TestSettings.Properties.Settings.Default, "mSize", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.DataBindings.Add(new System.Windows.Forms.Binding("BackColor", global::TestSettings.Properties.Settings.Default, "mColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.Location = global::TestSettings.Properties.Settings.Default.mLocation;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ TestSettings/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestSettings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            // -- In this example, the location, size, color of the window and text in TextEdit 
+            // -- are automatically saved. You must have the code to control the process.
+
+            
+            // -- this kod need if you want to control process
+
+            //textBox1.Text = Properties.Settings.Default.mText;
+            //Location = Properties.Settings.Default.mLocation;            
+            //Size = Properties.Settings.Default.mSize;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // -- this kod need if you want to control process
+
+            //Properties.Settings.Default.mLocation = this.Location;
+            //Properties.Settings.Default.mText = textBox1.Text;
+            //Properties.Settings.Default.mSize = this.Size;
+
+            // -- this must be done
+            Properties.Settings.Default.Save();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            BackColor = this.colorDialog1.Color;
+        }
+    }
+}
+
